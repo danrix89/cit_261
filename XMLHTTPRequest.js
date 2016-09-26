7e9d924934fdb0aba00beb67d34d0663084f6d15
Dan <danrix89@gmail.com>
2016-09-26 14:42:50 -0600

    XMLHTTPRequest (first commit - no code)

---
+++ XMLHTTPRequest.js
@@ -0,0 +1,23 @@
+/************************************
+
+    Sandbox: XMLHTTPRequest
+
+************************************/
+
+
+
+/************************************
+    Happy Paths
+************************************/
+    // Happy path (not using reviver)
+
+
+
+/************************************
+    Parse() Nasty Paths
+************************************/
+// Nasty path (Parsing with trailing commas)
+    try {
+    }
+    catch (exception) {
+    }
